fc04fbb85425bca78c514d95377d459d839bb6b9
HeXudong <xudonghe@zhulux.com>
2016-05-28 08:40:48 +0800

    试了一下 require css的效果

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import 'babel-polyfill';
 import cats from './cats';
 import $ from 'jquery';
-import './app.css';
+require('./app.css');
+//import './app.css'; // 效果跟require一样
 
 $("<h1>Catas</h1>").appendTo('body');
 const ul = $('<ul></ul>').appendTo('body');

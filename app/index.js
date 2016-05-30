47fb80d424b7a8cb3b619b8b9a81d08390ca5550
HeXudong <xudonghe@zhulux.com>
2016-05-30 10:18:14 +0800

    add todo app
    参考: http://cn.redux.js.org/docs/basics/ExampleTodoList.html

--- app/index.js
+++ app/index.js
@@ -3,6 +3,19 @@ require('./index.css');
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import HelloWorld from './components/HelloWorld';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './containers/App';
+import todoApp from './reducers';
 
-render(<HelloWorld />, document.getElementById('hello-world-application'));
+//import HelloWorld from './components/HelloWorld';
+//render(<HelloWorld />, document.getElementById('hello-world-application'));
+
+let store = createStore(todoApp);
+let rootElement = document.getElementById('root');
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
